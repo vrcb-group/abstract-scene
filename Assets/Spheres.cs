cea97918602f7a03f43571122c1ab35d7e33bff4
sgopala6 <sgopala6@ncsu.edu>
2020-01-23 10:15:36 +0100

    spheres and rods choice parameter

--- Assets/Spheres.cs
+++ Assets/Spheres.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class Spheres : MonoBehaviour
 {
     public GameObject Sphere;
+    public bool spheres = true;
     public int amount = 2000;
     List<GameObject> spherePool;
 
     void Start()
     {
-        spherePool = new List<GameObject>();
-
-        for (int i = 0; i < amount; i++)
+        if(spheres)
         {
-            GameObject sp = (GameObject)Instantiate(Sphere, new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-11.0f, 15.0f), Random.Range(-10.0f, 10.0f)), Quaternion.identity);
-            spherePool.Add(sp);
+            spherePool = new List<GameObject>();
+            for (int i = 0; i < amount; i++)
+            {
+                GameObject sp = (GameObject)Instantiate(Sphere, new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-11.0f, 15.0f), Random.Range(-10.0f, 10.0f)), Quaternion.identity);
+                spherePool.Add(sp);
+            }
+            StartCoroutine("BlinkSpheres");
         }
-
-        StartCoroutine("BlinkSpheres");
     }
 
     private IEnumerator BlinkSpheres()
