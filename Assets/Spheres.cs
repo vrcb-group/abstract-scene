c369c2fa4e14f353ac4f554be509c4ee3877f436
sgopala6 <sgopala6@ncsu.edu>
2020-03-02 15:43:52 +0100

    comments added to code

--- Assets/Spheres.cs
+++ Assets/Spheres.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Spheres : MonoBehaviour
 {
+    /// Initialize variables 
     public GameObject Sphere;
     public bool spheres = true;
     public int amount = 2000;
@@ -14,7 +15,8 @@ public class Spheres : MonoBehaviour
 
     void Start()
     {
-        if(spheres)
+        /// Display grid of random spheres if option is selected
+        if (spheres)
         {
             spherePool = new List<GameObject>();
             for (int i = 0; i < amount; i++)
@@ -22,9 +24,12 @@ public class Spheres : MonoBehaviour
                 GameObject sp = (GameObject)Instantiate(Sphere, new Vector3(Random.Range(-20.0f, 20.0f), Random.Range(-11.0f, 15.0f), Random.Range(-7.0f, 13.0f)), Quaternion.identity);
                 spherePool.Add(sp);
             }
-            if(limited_lifetime)
+
+            /// Run limited lifetime for spheres
+            if (limited_lifetime)
             {
-                switch(lifetime)
+                /// Set lifetime based on value specified
+                switch (lifetime)
                 {
                     case 150:
                         wait_time = 0.011f;
@@ -39,7 +44,9 @@ public class Spheres : MonoBehaviour
                         wait_time = 0;
                         break;
                 }
-                if(wait_time>0)
+
+                /// If valid lifetime value is specified, run coroutine to implement limited lifetime 
+                if (wait_time>0)
                 {
                     StartCoroutine("BlinkSpheres");
                 }               
@@ -50,50 +57,55 @@ public class Spheres : MonoBehaviour
 
     private IEnumerator BlinkSpheres()
     {
-        while(true)
+        /// Run until manual interruption
+        while (true)
         {
+            /// Set lifetime based on value specified
             loop_start = System.DateTime.Now.Millisecond;
+
+            /// Loop for entire set of spheres
             for (int i = 0; i < 10  ; i++)
             {
-                //loop_start = System.DateTime.Now.Millisecond;
+                /// Loop for every amount/10 spheres
                 for (int j = i*(amount/10); j < (i+1)*(amount/10); j++)
                 {
                     spherePool[j].SetActive(false);
                     spherePool[j].transform.position = new Vector3(Random.Range(-20.0f, 20.0f), Random.Range(-11.0f, 15.0f), Random.Range(-7.0f, 13.0f));
                     spherePool[j].SetActive(true);
-                    //if(j==300)
-                    //{
-                    //    Debug.Log(System.DateTime.Now.Millisecond);
-                    //}
                 }
-
-                //loop_end = System.DateTime.Now.Millisecond;
-
-                //Debug.Log(loop_end - loop_start);
-
-                //wait_time = (sub_factor - (loop_end - loop_start)) / 1000;
-
-                //if (wait_time > sub_factor/1000)
-                //{
-                //    wait_time = wait_time - 1;
-                //}
-
-                //Debug.Log(time_now);
-                //Debug.Log(System.DateTime.Now.Millisecond);
-                //Debug.Log(wait_time);   
-
-                Debug.Log(System.DateTime.Now.Millisecond);
-                yield return new WaitForSeconds(wait_time);
-                Debug.Log(System.DateTime.Now.Millisecond);
+                /// Wait for pre-decided time
+                yield return new WaitForSeconds(wait_time);               
             }
 
+            /// Calculate difference between start and end of first loop (all spheres) 
+            /// Roughly equal to the lifetime of each sphere
             diff = System.DateTime.Now.Millisecond - loop_start;
             if(diff < 0)
             {
                 diff = diff + 1000;
             }
-            //Debug.Log(diff);
+            Debug.Log(diff);
         }
     }
 
 }
+
+
+
+
+
+
+//loop_end = System.DateTime.Now.Millisecond;
+
+//Debug.Log(loop_end - loop_start);
+
+//wait_time = (sub_factor - (loop_end - loop_start)) / 1000;
+
+//if (wait_time > sub_factor/1000)
+//{
+//    wait_time = wait_time - 1;
+//}
+
+//Debug.Log(time_now);
+//Debug.Log(System.DateTime.Now.Millisecond);
+//Debug.Log(wait_time);   
